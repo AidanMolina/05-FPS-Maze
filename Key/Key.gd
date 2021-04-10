a38918566266fed79498bdb70e20d213977ee0b6
apmolina <apmolina@iu.edu>
2021-04-10 16:38:02 -0400

    Project 5 Push 1

---
+++ Key/Key.gd
@@ -0,0 +1,12 @@
+extends Area
+
+
+func _on_key_body_entered(body):
+	if body.name == "Player":
+		var exit = get_node_or_null("/root/Game/Maze/exit")
+		if exit != null:
+			var sound = get_node_or_null("/root/Game/Key")
+			if sound.playing != null:
+				sound.playing = true
+			exit.unlocked()
+			queue_free()
